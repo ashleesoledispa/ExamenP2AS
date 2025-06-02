e34ce5df6d921e400382009e3d33eedfe83517bc
Ashlee Soledispa PC <ashlee.soledispa@udla.edu.ec>
2025-06-02 08:50:31 -0500

    Se agregó pagina chiste y todo funciona correctamente

--- ExamenP2AS/Views/ChistePage.xaml.cs
+++ ExamenP2AS/Views/ChistePage.xaml.cs
@@ -1,9 +1,50 @@
+using System.Net.Http;
+using System.Text.Json;
+using Microsoft.Maui.Controls;
+
 namespace ExamenP2AS.Views;
 
 public partial class ChistePage : ContentPage
 {
-	public ChistePage()
-	{
-		InitializeComponent();
-	}
+    public ChistePage()
+    {
+        InitializeComponent();
+        CargarChiste();
+    }
+
+    private async void CargarChiste()
+    {
+        try
+        {
+            using HttpClient client = new();
+            string url = "https://official-joke-api.appspot.com/random_joke";
+            var response = await client.GetStringAsync(url);
+
+            var joke = JsonSerializer.Deserialize<Joke>(response);
+            if (joke != null)
+            {
+                JokeLabel.Text = $"{joke.setup}\n\n{joke.punchline}";
+            }
+            else
+            {
+                JokeLabel.Text = "No se pudo cargar el chiste.";
+            }
+        }
+        catch (Exception ex)
+        {
+            JokeLabel.Text = $"Error: {ex.Message}";
+        }
+    }
+
+    private void OnOtroChisteClicked(object sender, EventArgs e)
+    {
+        CargarChiste();
+    }
+}
+
+public class Joke
+{
+    public string type { get; set; }
+    public string setup { get; set; }
+    public string punchline { get; set; }
 }
